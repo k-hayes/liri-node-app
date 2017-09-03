cd808f4f1931e62235a914f35d5e7d7f39ecd977
Kate Hayes <kate.beaver@gmail.com>
2017-09-02 19:43:49 -0500

    JS files.

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'jj7IxFQCrmLyxP2uC8FrWKXoY',
+  consumer_secret: 'DegRtp1wt7CZ13C0Z7CoNV41JdjSUWlUfYiKnBg3fTPPhMveUM',
+  access_token_key: '904001706337464320-frjHayniLi2xhojyhOJkC62xqwWKp9W',
+  access_token_secret: 'DeTSDMwOderhYrATho6a9YBGjysd2BTE963phMUZ4X9TT',
+}
---
+++ liri.js
@@ -0,0 +1,1 @@
+var Keys = require("./keys.js");
